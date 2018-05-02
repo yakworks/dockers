6c61ac85fc0c3f9b5ac12d31bbca7c64d90d0efb
Joshua B <joshdev@9ci.com>
2018-05-02 12:29:39 -0600

    new gradle build

---
+++ gradle-4.6/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker build -t dock9/gradle:4.6 .
+docker push dock9/gradle:4.6
---
+++ oraclejdk8/gradle-4.6/build.sh
@@ -0,0 +1,2 @@
+docker build -t dock9/oraclejdk8:gradle-4.6 .
+docker push dock9/oraclejdk8:gradle-4.6
