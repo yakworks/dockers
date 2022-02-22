4f732998df4655f66e7babb87ae4e3ec1a7d6410
Joshua B <joshdev@9ci.com>
2022-02-22 16:51:33 -0700

    upgrade k8 and helm

--- builder/build.sh
+++ builder/build.sh
@@ -7,8 +7,15 @@ docker tag "$BASE_URL:3.14" "$BASE_URL:base"
 docker push "$BASE_URL:3.14"
 docker push "$BASE_URL:base"
 
-for t in k8s jdk8 jdk8-slim fat node14 node14-chrome postgres14-jdk8; do
+for t in k8s jdk8 jdk8-slim fat node node-chrome postgres14-jdk8; do
     echo "building ${BASE_URL}:${t}"
     docker build --build-arg REGISTRY=$REGISTRY -t "${BASE_URL}:${t}" "${t}/."
     docker push "${BASE_URL}:${t}"
 done
+
+# To only run one or 2 do this
+# for t in k8s node; do
+#     echo "building ${BASE_URL}:${t}"
+#     docker build --build-arg REGISTRY=$REGISTRY -t "${BASE_URL}:${t}" "${t}/."
+#     docker push "${BASE_URL}:${t}"
+# done
