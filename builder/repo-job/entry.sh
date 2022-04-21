633ee0fdc01f6d6b8d2d72686a9662d44c3b1daf
Joanna Dabal <joanna@9ci.com>
2022-04-21 09:32:17 -0500

    Update entry.sh

--- builder/repo-job/entry.sh
+++ builder/repo-job/entry.sh
@@ -18,7 +18,7 @@ GITHUB_BASE_URL="github.com/$GITHUB_PROJECT.git"
 GITHUB_URL="https://dummy:${GITHUB_TOKEN}@$GITHUB_BASE_URL"
 
 # set branch to master if not set
-: ${GITHUB_BRANCH:=master}
+: ${GITHUB_BRANCH:=repo-job}  #XXX change later to master
 
 # clone to /site
 git clone $GITHUB_URL git-project -b $GITHUB_BRANCH --single-branch --depth 1
