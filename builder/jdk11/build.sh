4b7024f775cd9834d6ee36b1bafd035edbf363dc
Joshua B <joshdev@9ci.com>
2022-06-19 17:20:08 -0600

    added builder/jdk11 images

---
+++ builder/jdk11/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+export REGISTRY=yakworks
+BASE_URL=$REGISTRY/builder
+TAG=jdk11
+
+export DOCKER_DEFAULT_PLATFORM=linux/amd64  
+
+docker build --build-arg REGISTRY=$REGISTRY -t "${BASE_URL}:${TAG}" .
+docker push "${BASE_URL}:${TAG}"
+
---
+++ builder/node-jdk11/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+export REGISTRY=yakworks
+BASE_URL=$REGISTRY/builder
+NAME=node-jdk11
+
+# only needed when doing this on Mac Silicon(m1)
+export DOCKER_DEFAULT_PLATFORM=linux/amd64  
+
+docker build --build-arg REGISTRY=$REGISTRY -t "${BASE_URL}:${NAME}" .
+docker push "${BASE_URL}:${NAME}"
+
