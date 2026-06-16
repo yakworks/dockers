7b94b649701e568b420749b1aab0b426304f07a0
jdabal <joanna@9ci.com>
2026-06-15 23:02:02 -0500

    joanna@9ci.com
    * Update docker-entrypoint-initdb.sh
    
    * Update docker-entrypoint-initdb.sh
    
    * Update docker-entrypoint-initdb.sh

--- mssql-server/2019/docker-entrypoint-initdb.sh
+++ mssql-server/2019/docker-entrypoint-initdb.sh
@@ -24,17 +24,29 @@ for f in /docker-entrypoint-initdb.d/*; do
         #remove .bak ext
         DBNAME="${BAKFILE%.bak}"
         echo "$0: RESTORING $BAKFILE"
+
+        # Check if backup has secondary data file ndf (large databases ith backup taken with Windows will have that)
+        HAS_NDF=$(sqlcmd -C -U sa -P "$SA_PASSWORD" -Q "RESTORE FILELISTONLY FROM DISK = '$f'" -h -1 2>/dev/null | grep -c "${DBNAME}_Data_01")
+
         RST="RESTORE DATABASE $DBNAME FROM DISK = '$f' "
-        RST+="WITH REPLACE, FILE = 1, NOUNLOAD, STATS = 5, MOVE '$DBNAME' TO '/var/opt/mssql/data/${DBNAME}.mdf', "
-        RST+="MOVE '${DBNAME}_Data_01' TO '/var/opt/mssql/data/${DBNAME}.ndf', "
+        RST+="WITH REPLACE, FILE = 1, NOUNLOAD, STATS = 5, "
+        RST+="MOVE '$DBNAME' TO '/var/opt/mssql/data/${DBNAME}.mdf', "
+
+        if [ "$HAS_NDF" -gt 0 ]; then
+            echo "$0: Secondary data file detected, adding NDF move"
+            RST+="MOVE '${DBNAME}_Data_01' TO '/var/opt/mssql/data/${DBNAME}.ndf', "
+        fi
+
         RST+="MOVE '${DBNAME}_log' TO '/var/opt/mssql/data/${DBNAME}_log.ldf'"
+
         echo "$0: $RST"
         sqlcmd -C -U sa -P "$SA_PASSWORD" -Q "$RST" ;;
-    *.sql)    echo "$0: running $f"; sqlcmd -C -U SA -P "$SA_PASSWORD" -X -i  "$f"; echo ;;
-    *.sql.gz) echo "$0: running $f"; gunzip "$f" ; sqlcmd -C -U SA -P "$SA_PASSWORD" -X -i  "${f%.gz}"; echo ;;
+    *.sql)    echo "$0: running $f"; sqlcmd -C -U SA -P "$SA_PASSWORD" -X -i "$f"; echo ;;
+    *.sql.gz) echo "$0: running $f"; gunzip "$f"; sqlcmd -C -U SA -P "$SA_PASSWORD" -X -i "${f%.gz}"; echo ;;
     *)        echo "$0: ignoring $f" ;;
   esac
   echo
 done
+
 echo "$0: SQL Server Database ready, listing .."
 sqlcmd -C -U SA -P "$SA_PASSWORD" -Q 'SELECT Name FROM sys.Databases'
